ce4315234dd22f5c8be04980266f14bd08893454
nsachin08 <naiksci90@gmail.com>
2024-07-30 22:49:27 +0530

    Step5.Creating User-Player

---
+++ Tic-Tac-Toe/Player.java
@@ -0,0 +1,26 @@
+public class Player {
+
+    public String name;
+    public PlayingPiece playingPiece;
+
+    public Player(String name,PlayingPiece playingPiece){
+        this.name = name;
+        this.playingPiece = playingPiece;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayingPiece getPlayingPiece(){
+        return this.playingPiece;
+    }
+
+    public void setPlayingPiece(PlayingPiece playingPiece){
+        this.playingPiece = playingPiece;
+    }
+}
