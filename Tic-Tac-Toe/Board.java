2f05c51ebcae716016cb791fa30e4f9642125528
nsachin08 <naiksci90@gmail.com>
2024-07-30 22:41:16 +0530

    4. Creating Board

--- Tic-Tac-Toe/Board.java
+++ Tic-Tac-Toe/Board.java
@@ -2,4 +2,11 @@ public class Board {
 
     public int size;
     public PlayingPiece[][] board;
+
+    public Board(int size){
+        this.size = size;
+        board = new PlayingPiece[size][size];
+    }
+    
+
 }
