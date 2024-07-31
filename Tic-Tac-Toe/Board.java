0ec788d07fbd07a748925ee604a043d52cda1e7a
nsachin08 <naiksci90@gmail.com>
2024-07-31 12:52:02 +0530

    Step6. Updating Board

--- Tic-Tac-Toe/Board.java
+++ Tic-Tac-Toe/Board.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
 
     public int size;
@@ -6,6 +9,32 @@ public class Board {
     public Board(int size){
         this.size = size;
         board = new PlayingPiece[size][size];
+    }
+    public List<Pair<Integer,Integer>> getFreeCells(){
+        List<Pair<Integer,Integer>> freeCells = new ArrayList<>();
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(board[i][j] == null){
+                    Pair<Integer,Integer> rowColumn = new Pair<>(i,j);
+                    freeCells.add(rowColumn);
+                }
+            }
+        }
+        return freeCells;
+    }
+
+    public void printBoard(){
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(board[i][j] != null){
+                    System.out.print(board[i][j].pieceType.name() + " ");
+                }else{
+                    System.out.println("   ");
+                }
+                System.out.println(" | ");
+            }
+            System.out.println();
+        }
     }
     
 
