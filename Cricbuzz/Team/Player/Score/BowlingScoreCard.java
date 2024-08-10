9bb0e4d10bc0f7ad865861a0c7e4559785e7d1ad
Sachin Naik <sachinnaik@Sachins-Laptop.local>
2024-08-10 18:16:02 +0530

    Adding Bowler/Batsman ScoreCard

---
+++ Cricbuzz/Team/Player/Score/BowlingScoreCard.java
@@ -0,0 +1,11 @@
+
+public class BowlingScoreCard{
+
+    public int totalOversCount;
+    public int runsGiven;
+    public int wicketsTaken;
+    public int noBallCount;
+    public int wideBallCount;
+    public double economyRate;
+
+}
