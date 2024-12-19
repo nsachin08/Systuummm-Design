f8474175d2225894f296ebc6e70570ca096fcd8b
Sachin Naik <sachinnaik@Sachins-Laptop.local>
2024-12-19 19:47:12 +0530

    Add Interface Segregation Principle

---
+++ SOLID/ISP/BadCode/MultiPurposeMachine.java
@@ -0,0 +1,17 @@
+public class MultiPurposeMachine implements Machine{
+
+    @Override 
+    public void print(Document doc){
+        System.out.println("Printing document...");
+    }
+
+    @Override 
+    public void scan(Document doc){
+        System.out.println("Scanning document...");
+    }
+
+    @Override 
+    public void copy(Document doc){
+        System.out.println("Copying document...");
+    }
+}
